d62973ee2575b60c4d446069d04e9b206a10881e
ashuroov <ashurovabdulaziz588@gmail.com>
2022-11-15 20:52:35 +0500

    Something

---
+++ Lesson_20/lesson_20.py
@@ -0,0 +1,33 @@
+'''
+git config --global user.name "name in github"
+git config --global user.email "real email"
+
+System:
+git config --system
+Use: git config --global
+Project: git config
+
+
+git config --list: выводит все наастройки конфигурации
+git config user.name: чтобы вывести определенную конфигурацию
+git config --global core.editor - to set editor to open
+
+git init - создание локального пустого репозитория с .git файлом
+git status
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ashuroov/FSPR-422.git
+
+
+Урок 20
+fix: Adds lesson_20.py and materials.md
+bug:
+upd:
+del:
+'''
+
+a = 1
+b = 2
+s = a + b
+print(s)
